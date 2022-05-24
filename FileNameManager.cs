a07d680d012cbdc984ed0201ebe3e4ac8f8692ad
itLezy <itLezy@users.noreply.github.com>
2022-05-24 19:32:24 +0200

    OK

---
+++ FileNameManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ILCommon
+{
+    public class FileNameManager
+    {
+        public String NormalizeFileName (String originalName)
+        {
+            return originalName
+                .Replace ("-[rarbg.to]", "")
+                .Replace ("[rarbg]", "");
+        }
+    }
+}
