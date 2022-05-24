b75d83c64e80ae04ee68ad182b08707cd6364abc
itLezy <itLezy@users.noreply.github.com>
2022-05-25 00:57:02 +0200

    OK

--- FileNameManager.cs
+++ FileNameManager.cs
@@ -21,12 +21,29 @@ namespace ILCommon
             var latinc = 0;
 
             foreach (char c in cc) {
-                if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+                if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || '.' == c || (c >= '0' && c <= '9')) {
                     latinc++;
                 }
             }
 
             return ((double) latinc / (double) cc.Length) > 0.5;
+        }
+
+        public String GetSubCat (String fileName)
+        {
+            var subCat = "generic";
+
+            if (fileName.Contains ("480") || fileName.Contains ("SD")) {
+                subCat = "D0480p";
+            } else if (fileName.Contains ("720")) {
+                subCat = "D0720p";
+            } else if (fileName.Contains ("1080") || fileName.ToLower ().Contains ("fhd")) {
+                subCat = "D1080p";
+            } else if (fileName.Contains ("2160") || fileName.ToLower ().Contains ("4k")) {
+                subCat = "D2160p";
+            }
+
+            return subCat;
         }
     }
 }
